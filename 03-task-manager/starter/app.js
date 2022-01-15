17a561d297c09ab8ab56e4d7866c66433f750cef
shreejitverma <shreejiv@andrew.cmu.edu>
2022-01-15 22:04:01 +0530

    module update
    module update

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,1 +1,20 @@
-console.log('Task Manager App')
+const express = require('express');
+const { route } = require('./routes/tasks');
+const app = express()
+const tasks = require('./routes/tasks')
+
+
+//middleware
+
+app.use(express.json())
+
+
+
+// routes
+app.get('/hello', (req, res) => {
+    res.send('Task Manager App')
+})
+app.use('/api/v1/tasks', tasks)
+// console.log('Task Manager App')
+const port = 3000
+app.listen(port, console.log(`Server is listening on port ${port}`));
